8a9799e09d233e09932d77295e1b7dcfa1aa29bd
mosta82 <74375862+mosta82@users.noreply.github.com>
2025-05-04 15:48:29 +0600

    Add files via upload

---
+++ prob1.py
@@ -0,0 +1,20 @@
+age = int(input("how old are u?"))
+
+
+if age<0:
+    print("Invalid")
+
+elif age<5:
+    print("free ride")
+
+elif age<=17:
+    print("Child Fare")
+
+elif age<=59:
+    print("Adult Fare ")
+
+
+
+
+else:
+    print("Senior citezen Fare")
